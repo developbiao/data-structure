c4976aa6d441ed874922973e99da3b6ec89585eb
Gong Biao <ggongbiao@gamil.com>
2020-07-26 14:15:25 +0800

    Queues comparison benchmark

--- LoopQueue/src/Main.java
+++ LoopQueue/src/Main.java
@@ -1,16 +1,38 @@
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-        LoopQueue<Integer> queue = new LoopQueue<>();
+        int opCount = 100000;
 
-        for (int i = 0; i < 10; i ++) {
-            queue.enqueue(i);
-            System.out.println(i);
+        ArrayQueue<Integer> arrayQueue = new ArrayQueue<>();
+        double time1 = benchmark(arrayQueue, 0);
+        System.out.println("Array Queue spend time: " + time1 + " s");
 
-            if (i % 3 == 2) {
-                queue.dequeue();
-                System.out.println(queue);
-            }
+        LoopQueue<Integer> loopQueue = new LoopQueue<>();
+        double time2 = benchmark(loopQueue, opCount);
+        System.out.println("Loop Queue spend time: " + time2 + " s");
+
+
+    }
+
+    private static double benchmark(Queue<Integer> queue, int opCount)
+    {
+        long startTime = System.nanoTime();
+
+        Random random = new Random();
+        // enqueue
+        for(int i = 0; i < opCount; i ++) {
+            queue.enqueue(random.nextInt(Integer.MAX_VALUE));
         }
+
+        // dequeue
+        for(int i = 0; i < opCount; i ++) {
+            queue.dequeue();
+        }
+
+        long endTime = System.nanoTime();
+
+        return (endTime - startTime) / 1000000000.0;
     }
 }
