ea8db2eef0936382b17879b0e5531b3e74c115b7
Gong Biao <ggongbiao@gamil.com>
2020-08-09 15:05:51 +0800

    How to test your linked list problem solution

--- LinkedListProblemInLeetCode/src/ListNode.java
+++ LinkedListProblemInLeetCode/src/ListNode.java
@@ -1,12 +1,36 @@
 public class ListNode {
-      int val;
-      ListNode next;
-      ListNode() {}
-      ListNode(int val) {
+      public int val;
+      public ListNode next;
+      public ListNode() {}
+      public ListNode(int val) {
             this.val = val;
       }
-      ListNode(int val, ListNode next) {
+      public ListNode(int val, ListNode next) {
             this.val = val;
             this.next = next;
       }
+
+      public ListNode(int[] arr) {
+          if(arr == null || arr.length == 0){
+             throw new IllegalArgumentException("Array can not be empty.");
+          }
+          this.val = arr[0];
+          ListNode cur = this;
+          for(int i = 1; i < arr.length; i ++) {
+              cur.next = new ListNode(arr[i]);
+              cur = cur.next;
+          }
+      }
+
+     @Override
+    public String toString() {
+          StringBuilder builder = new StringBuilder();
+          ListNode cur = this;
+          while(cur != null) {
+             builder.append(cur.val + "->") ;
+             cur = cur.next;
+          }
+          builder.append("NULL");
+          return builder.toString();
+    }
 }
--- LinkedListProblemInLeetCode/src/Main.java
+++ LinkedListProblemInLeetCode/src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int[] arr = {1, 2, 3,  4, 5, 6, 7, 7, 7, 8};
+        ListNode listNode = new ListNode(arr);
+        System.out.println("Before list node");
+        System.out.println(listNode);
+        Solution solution = new Solution();
+        solution.removeElements(listNode, 7);
+        System.out.println("After list node");
+        System.out.println(listNode);
     }
 }
