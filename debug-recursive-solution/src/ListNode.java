6863c7908d14b645225be4b2cef2072e197fb679
Gong Biao <ggongbiao@gamil.com>
2020-08-19 13:51:21 +0800

    Learning Debug link list recursive

---
+++ debug-recursive-solution/src/ListNode.java
@@ -0,0 +1,36 @@
+public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode() {}
+      public ListNode(int val) {
+            this.val = val;
+      }
+      public ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+      }
+
+      public ListNode(int[] arr) {
+          if(arr == null || arr.length == 0){
+             throw new IllegalArgumentException("Array can not be empty.");
+          }
+          this.val = arr[0];
+          ListNode cur = this;
+          for(int i = 1; i < arr.length; i ++) {
+              cur.next = new ListNode(arr[i]);
+              cur = cur.next;
+          }
+      }
+
+     @Override
+    public String toString() {
+          StringBuilder builder = new StringBuilder();
+          ListNode cur = this;
+          while(cur != null) {
+             builder.append(cur.val + "->") ;
+             cur = cur.next;
+          }
+          builder.append("NULL");
+          return builder.toString();
+    }
+}
---
+++ debug-recursive-solution/src/Solution.java
@@ -0,0 +1,39 @@
+public class Solution {
+    public ListNode removeElements(ListNode head, int val,  int depth) {
+        String depthString = generateDepthString(depth);
+        System.out.print(depthString);
+
+        System.out.println("Call: remove " + val + " in " + head);
+
+        if(head == null) {
+            System.out.print(depthString);
+            System.out.println("Return: " + head);
+            return null;
+        }
+
+        ListNode res = removeElements(head.next, val, ++depth);
+        System.out.print(depthString);
+        System.out.println("After remove " + val + ": " + res);
+
+        ListNode ret;
+        if(head.val == val) {
+           ret = res;
+        }else{
+            head.next = res;
+            ret = head;
+        }
+
+        System.out.print(depthString);
+        System.out.println("Return: " + ret);
+
+        return ret;
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for(int i = 0; i < depth; i ++) {
+            res.append("--");
+        }
+        return res.toString();
+    }
+}
